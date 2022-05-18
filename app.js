110fda759409785ed89e843b6f08675c25df2174
Arkrez <draross2019@gmail.com>
2022-05-17 20:28:56 -0700

    Making the code cleaner/modular

--- app.js
+++ app.js
@@ -1,92 +1,112 @@
-const openModalButtons = document.querySelectorAll("[data-modal-target]");
-const closeModalButtons = document.querySelectorAll("[data-close-button]");
-const overlay = document.getElementById('overlay');
-const addBook = document.querySelector(".create-book");
-const library = document.querySelector(".library");
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const hasRead = document.getElementById('hasRead');
+class Book {
+    constructor(title, author, read)
+    {
+        this.title = title;
+        this.author = author;
+        this.read = read;
+    }
+}
 
-openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget);
-        openModal(modal);
+const LibraryMod = (()=>{
+    LibraryController = {};
+    const addBook = document.querySelector(".create-book");
+    const library = document.querySelector(".library");
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const hasRead = document.getElementById('hasRead');
+    addBook.addEventListener('click', () => {
+        const newBook = new Book(title.value, author.value, hasRead.checked? "Read":"Not Read")
+        CreateBookCard(newBook);
+        resetValues();
     });
-});
-
-overlay.addEventListener('click', () =>{
-    const modals = document.querySelectorAll('.modal.active');
-    modals.forEach(modal => closeModal(modal));
-})
-
-closeModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = button.closest('.modal');
-        closeModal(modal);
-    });
-});
-
-addBook.addEventListener('click', () => {
-    CreateBookCard(title.value, author.value, hasRead.checked? "Read":"Not Read");
-    resetValues();
-});
-function resetValues(){
-    title.value = "";
-    author.value = "";
-    hasRead.checked = false;
-}
-
-
-function openModal(modal){
-    if(modal === null) return;
-    modal.classList.add('active');
-    overlay.classList.add('active');
-}
-function closeModal(modal){
-    if(modal === null) return;
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
-}
-
-
-function CreateBookCard(title, author, read){
+    function resetValues(){
+        title.value = "";
+        author.value = "";
+        hasRead.checked = false;
+    }
     
-    const bookCard = document.createElement("div");
-    const bTitle = document.createElement("div");
-    const bAuthor = document.createElement("div");
-    const bHasRead = document.createElement("button");
-    const bRemove = document.createElement("button");
-    if(title == ''|| author == '') return;
-    bookCard.classList.add("book-card");
-    bTitle.textContent = `${title}`;
-    bTitle.classList.add("title");
-    bAuthor.classList.add("author");
-    if(read.toLowerCase() == "read")
-        bHasRead.classList.add("read");
-    else
-        bHasRead.classList.add("not-read");
-    bRemove.classList.add("remove-card");
-    bHasRead.addEventListener('click', () =>{
+    function CreateBookCard(newBook){
+        const bookCard = document.createElement("div");
+        const bTitle = document.createElement("div");
+        const bAuthor = document.createElement("div");
+        const bHasRead = document.createElement("button");
+        const bRemove = document.createElement("button");
 
-        if(bHasRead.textContent.toLowerCase() == "read"){
-            bHasRead.classList.remove("read");
-            bHasRead.textContent = "Not Read";
-            bHasRead.classList.add("not-read")
-        }
-        else
-        {
-            bHasRead.classList.remove("not-read");
-            bHasRead.textContent = "Read";
+        if(newBook.title == ''|| newBook.author == '') return;
+
+        bookCard.classList.add("book-card");
+        bTitle.classList.add("title");
+        bAuthor.classList.add("author");
+
+        if(newBook.read.toLowerCase() == "read")
             bHasRead.classList.add("read");
-        }
-            
+        else
+            bHasRead.classList.add("not-read");
+        bRemove.classList.add("remove-card");
+        
+        bHasRead.addEventListener('click', () =>{
+
+            if(bHasRead.textContent.toLowerCase() == "read"){
+                bHasRead.classList.remove("read");
+                bHasRead.textContent = "Not Read";
+                bHasRead.classList.add("not-read")
+            }
+            else
+            {
+                bHasRead.classList.remove("not-read");
+                bHasRead.textContent = "Read";
+                bHasRead.classList.add("read");
+            }
+                
+        });
+        bRemove.addEventListener('click', () =>{
+            bookCard.remove();
+        });
+        bTitle.textContent = `${newBook.title}`;
+        bAuthor.textContent = newBook.author;
+        bHasRead.textContent = newBook.read;
+        bRemove.textContent = 'Remove';
+        bookCard.append(bTitle, bAuthor, bHasRead, bRemove);
+        library.appendChild(bookCard);
+    }
+    
+    return LibraryController;
+
+})();
+
+const ModalMod = (()=>{
+    ModalController = {};
+
+    const openModalButtons = document.querySelectorAll("[data-modal-target]");
+    const closeModalButtons = document.querySelectorAll("[data-close-button]");
+    const overlay = document.getElementById('overlay');
+    openModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = document.querySelector(button.dataset.modalTarget);
+            openModal(modal);
+        });
     });
-    bRemove.addEventListener('click', () =>{
-        bookCard.remove();
+    
+    overlay.addEventListener('click', () =>{
+        const modals = document.querySelectorAll('.modal.active');
+        modals.forEach(modal => closeModal(modal));
+    })
+    
+    closeModalButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const modal = button.closest('.modal');
+            closeModal(modal);
+        });
     });
-    bAuthor.textContent = `${author}`;
-    bHasRead.textContent = `${read}`;
-    bRemove.textContent = 'Remove';
-    bookCard.append(bTitle, bAuthor, bHasRead, bRemove);
-    library.appendChild(bookCard);
-}
+    function openModal(modal){
+        if(modal === null) return;
+        modal.classList.add('active');
+        overlay.classList.add('active');
+    }
+    function closeModal(modal){
+        if(modal === null) return;
+        modal.classList.remove('active');
+        overlay.classList.remove('active');
+    }
+    return ModalController;
+})();
